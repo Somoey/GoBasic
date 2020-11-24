e7890f281e912d05fb0f25cb3549d7f415af470a
mengyang <mengyang.sun@thoughtworks.com>
2020-11-24 21:03:52 +0800

    define const

--- basic.go
+++ basic.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+//常量可以用len(), cap(), unsafe.Sizeof()函数计算表达式的值。常量表达式中，函数必须是内置函数
+const (
+	c1 = "abc"
+	c2 = len(c1)
+	c3 = unsafe.Sizeof(c2)
+)
 
 func main() {
 
@@ -15,4 +25,15 @@ func main() {
 	// bool 零值为 false
 	var c bool
 	fmt.Println(c)
+
+	var d = 100
+	fmt.Println(d)
+
+	f := 1000
+	fmt.Println(f)
+
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Println(c3)
+
 }
